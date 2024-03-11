525e03f1c27b76300e81c084272a2edf75d87e67
mzarabieh <137957383+mzarabieh@users.noreply.github.com>
2024-03-11 13:57:04 -0400

    LibraryItem.java

--- items/LibraryItem.java
+++ items/LibraryItem.java
@@ -15,33 +15,87 @@ public abstract class LibraryItem {
     protected boolean purchasable;
     protected String title;
 
+    private boolean isRented = false;
+    private LocalDate rentalDate;
+    private LocalDate dueDate;
+    private String rentedBy; // Updated to use instead of borrowerEmail
+
     //req8 - fardad
     protected String borrowerEmail; // New field to store the email of the borrower
 
-    public LibraryItem(String itemType, String location, boolean purchasable) {
-        this.itemId = nextId++; 
+   public LibraryItem(String itemType, String location, boolean purchasable, String title) {
+        this.itemId = nextId++;
         this.itemType = itemType;
         this.location = location;
         this.purchasable = purchasable;
+        this.title = title; 
+    }
         
         // req 8 - fardad
         this.borrowerEmail = ""; // Initialize borrowerEmail to null
         this.title = title; // added 
     }
 
+// Methods to rent and return items, including updating the database
+    public void rentItem(LocalDate startDate, int rentalPeriodDays, String userEmail) {
+        this.isRented = true;
+        this.rentalDate = startDate;
+        this.dueDate = startDate.plusDays(rentalPeriodDays);
+        this.rentedBy = userEmail;
+        updateDatabase();
+    }
 
+    public void returnItem() {
+        this.isRented = false;
+        this.rentalDate = null;
+        this.dueDate = null;
+        this.rentedBy = null;
+        updateDatabase();
+    }
  
     public String getTitle() {
     	return this.title;
     }
     
+ // Update the database with the item's rental status
+    private void updateDatabase() {
+        // Path to your CSV database
+        Path path = Paths.get("path/to/library.csv");
+        try {
+            List<String> fileContent = new ArrayList<>(Files.readAllLines(path));
+            for (int i = 0; i < fileContent.size(); i++) {
+                if (fileContent.get(i).startsWith(String.valueOf(itemId))) {
+                    String[] splitLine = fileContent.get(i).split(",");
+                    splitLine[5] = Boolean.toString(isRented);
+                    splitLine[6] = isRented ? rentalDate.toString() : "";
+                    splitLine[7] = isRented ? dueDate.toString() : "";
+                    splitLine[8] = rentedBy != null ? rentedBy : "";
+                    fileContent.set(i, String.join(",", splitLine));
+                    break;
+                }
+            }
+            Files.write(path, fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    // req 8 - fardad
-     @Override
-	public String toString() {
-		return "LibraryItem [itemId=" + itemId + ", itemType=" + itemType + ", location=" + location + ", purchasable="
-				+ purchasable + ", borrowerEmail=" + borrowerEmail + ", title=" + title + "]";
-	}
+    // Existing methods...
+    @Override
+    public String toString() {
+        // Update toString() to include new rental attributes
+        return String.format("LibraryItem [itemId=%d, itemType=%s, location=%s, purchasable=%b, title=%s, isRented=%b, rentalDate=%s, dueDate=%s, rentedBy=%s]",
+                itemId, itemType, location, purchasable, title, isRented, rentalDate, dueDate, rentedBy);
+    }
+
+    public abstract String getDetails();
+
+ //    // req 8 - fardad
+ //     @Override
+	// public String toString() {
+	// 	return "LibraryItem [itemId=" + itemId + ", itemType=" + itemType + ", location=" + location + ", purchasable="
+	// 			+ purchasable + ", borrowerEmail=" + borrowerEmail + ", title=" + title + "]";
+	// }
 
 
     public abstract String getDetails();

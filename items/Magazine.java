54b4062ab4b4947dcaa91b19087be8941f430a2a
216941692 <123399042+216941692@users.noreply.github.com>
2024-03-11 16:51:17 -0400

    Update Magazine.java

--- items/Magazine.java
+++ items/Magazine.java
@@ -3,12 +3,10 @@ package items;
 //@author: Mehregan Mesgari
 
 class Magazine extends LibraryItem {
-    private String title;
     private String publisher;
 
     public Magazine(String location, boolean purchasable, String title,String publisher) {
-        super("Magazine",location, purchasable);
-        this.title = title;
+        super("Magazine",location, purchasable, title);
         this.publisher = publisher; 
     }
 
@@ -16,9 +14,7 @@ class Magazine extends LibraryItem {
     public String getDetails() {
         return "Magazine: " + title + ". Location: " + getLocation() + ". Purchasable: " + isPurchasable();
     }
-    public String getTitle() {
-    	return this.title;
-    }
+
     public String getPublisher() {
     	return this.publisher;
     }
